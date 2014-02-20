e3366144b981606f6a3fac96e36b07880295fcd4
Juan Jesús <metallitico@gmail.com>
2014-02-20 12:31:47 -0600

    some changes

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,3 +1,3 @@
-<?php
+<php
 require_once "paths.php";
 require_once $paths["vendor"] . "autoload.php";
---
+++ vendor/autoload.php
@@ -0,0 +1,9 @@
+<?php
+var libs = array(
+	"chess" => "ChessGame.php",
+	"dbManager" => "DBManager",
+	"exception" => "autoload",
+	"fenom"     => "Fenom",
+	"helper"    => "autoload"
+);
+
