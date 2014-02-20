41dcc0f0ea4714b676041c5f67d97cc24bfcd038
Juan Jesús <metallitico@gmail.com>
2014-02-20 12:03:58 -0600

    some changes

--- app.php
+++ app.php
@@ -1,4 +1,6 @@
 <?php
+require_once "bootstrap/autoload.php";
+
 $config["app"] = require("config/app.config.php");
 $config["database"] = require("config/database.config.php");
 require "vendor/DBConector/autoload.php";
--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,1 +1,2 @@
 <?php
+echo realpath(__FILE__;
