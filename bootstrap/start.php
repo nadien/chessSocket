12cb34119ed98c8f5dfcee894d7e721d610969d0
Juan Jesús <metallitico@gmail.com>
2014-02-23 18:33:18 -0600

    some changes

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -6,5 +6,5 @@ require_once "paths.php";
 require_once "loadConfig.php";
 
 require_once $paths["vendor"] . "autoload.php";
-require_once $paths["app"]    . "autoload.php";
 
+$app = chessSocket\classes\App::getInstance();
